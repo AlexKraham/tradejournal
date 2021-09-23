eb33bad90f22737cd18f64d77ac32152dc93a412
Alexander <alex@kraham.co>
2021-09-23 08:31:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+import { Auth } from 'aws-amplify'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Test</h1>
+        <AmplifySignOut />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default withAuthenticator(App)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+
+// import { withAuthenticator } from '@aws-amplify/ui-react'
+// const AppWithAuth = withAuthenticator(App)
+
+// const federated = {
+//   googleClientId:
+//     '563408246440-qkriha85adpecvbds5o6780jee2i4473.apps.googleusercontent.com',
+// }
+
+Amplify.configure(config)
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
